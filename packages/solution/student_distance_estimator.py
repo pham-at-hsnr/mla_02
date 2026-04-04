87ebaa3341d8fa5c263b4d37bbaca57a55986ad9
Duc Duy Pham <ducduy.pham@outlook.com>
2026-04-04 03:42:03 +0200

    fix

--- packages/solution/student_distance_estimator.py
+++ packages/solution/student_distance_estimator.py
@@ -4,9 +4,12 @@ from .config import DISTANCE_MODEL_PATH
 
 class DistanceEstimator:
     def __init__(self):
-        self.numpy_weights = np.load(DISTANCE_MODEL_PATH)
+        try:
+            self.numpy_weights = np.load(DISTANCE_MODEL_PATH)
+        except FileNotFoundError:
+            print("Distance model weights not found.")
 
     def estimate_distance(self, pixel_width, pixel_height):
         # TODO implement the distance estimation logic here
         # use self.numpy_weights to access the trained weights of your model
-        pass
+        return -1
